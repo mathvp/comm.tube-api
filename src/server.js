5f1edeb94aafffb1f674aeaf8f154edee6c12f53
Matheus Vieira <matheusvieiradepaula@gmail.com>
2021-01-04 00:55:08 -0300

    Edit CORS options

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 require('./database');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 require('./routes/auth.routes')(app);
